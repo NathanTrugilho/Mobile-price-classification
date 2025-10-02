78a50570b7bbc411e9ab820260020d35fa3b64be
Nathan Trugilho Braga <trugicraft@gmail.com>
2025-10-01 21:17:58 -0300

    Fiz tudo
    Criei a matriz de confusão, fiz o código que divide o dataset, defini o mlp e seus parâmetros, rodei e gerei a matriz de confusão

---
+++ functions/split_dataset.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+def split_dataset(selected_features=None):
+    df = pd.read_csv('dataset/mobile_price_classification_dataset.csv')
+    
+    target_column = 'price_range' #Coluna q vou prever
+
+    if selected_features is not None:
+        X = df[selected_features]
+    else:
+        X = df.drop(target_column, axis=1) # Se eu não definir nada, divide usando todos os parâmetros do dataset
+
+    y = df[target_column]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.3,
+        random_state=28,
+        stratify=y,
+    )
+
+    return X_train, X_test, y_train, y_test
